d7901047cf052faee9fbb1ad730d54601e5481c6
Alex GG <alex13gro@gmail.com>
2025-11-27 13:51:58 -0600

    update app check boxes

--- app.py
+++ app.py
@@ -3,23 +3,20 @@ import plotly.express as px
 import streamlit as st
 
 st.header('Análisis de Vehiculos')
-     
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+
+# Casilla para construir un histograma
+build_histogram = st.checkbox('Construir un histograma')
      
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
+if build_histogram:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-         
-    # crear un histograma
     fig = px.histogram(car_data, x="odometer")
-     
-    # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-# Botón para scatter plot
-scatter_button = st.button('Construir diagrama de dispersión')
-if scatter_button:
+# Casilla para construir un diagrama de dispersión
+build_scatter = st.checkbox('Construir un diagrama de dispersión')
+
+if build_scatter:
     st.write('Creación de un gráfico de dispersión: odometer vs price')
     fig2 = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig2, use_container_width=True)
